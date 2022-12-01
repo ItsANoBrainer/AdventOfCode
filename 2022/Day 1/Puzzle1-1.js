c440ae4f075cad851c655c525a28869b3fc3a075
ItsANoBrainer <itsanobrainergg@protonmail.com>
2022-12-01 01:15:36 -0500

    2022 Day 1

---
+++ 2022/Day 1/Puzzle1-1.js
@@ -0,0 +1,39 @@
+/* https://adventofcode.com/2022/day/1 */
+const path = require('path');
+const fs = require('fs');
+
+const input = fs
+    .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+    .toString()
+    .trim()
+    .split('\r\n');
+
+start(input)
+function start(input) {
+    // Make each array index a number instead of string. Empty lines make undefined for later.
+    input = input.map(x => parseInt(x) || undefined)
+
+    // Loop through all input. Make an array of each number until undefined is hit, then take that array and push it to the totals array.
+    let totals = []
+    let tempTotal = []
+    input.forEach(num => {
+        if (num == undefined) {
+            totals.push(tempTotal)
+            tempTotal = []
+        } else {
+            tempTotal.push(num)
+        }
+    })
+
+    // Take the total of each index's array and make that array of totals the new array
+    totals = totals.map(totalArray => {
+        let total = 0
+        totalArray.forEach(num => total += num)
+        return total
+    })
+
+    // Sort by highest to lowest
+    totals.sort(function (a, b) { return b - a })
+
+    console.log(`Highest: ${totals[0]}`)
+}
---
+++ 2022/Day 24/Puzzle24-2.js
@@ -0,0 +1,14 @@
+/* https://adventofcode.com/2022/day24#part2 */
+const path = require('path');
+const fs = require('fs');
+
+const input = fs
+	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+	.toString()
+	.trim()
+	.split('\r\n');
+
+start(input)
+function start(input) {
+
+}
