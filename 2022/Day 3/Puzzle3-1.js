b70f34da5f4045bbb1f3f7e3881a19bef757bb5e
ItsANoBrainer <itsanobrainergg@protonmail.com>
2022-12-03 15:00:56 -0500

    Day 3

--- 2022/Day 3/Puzzle3-1.js
+++ 2022/Day 3/Puzzle3-1.js
@@ -10,5 +10,21 @@ const input = fs
 
 start(input)
 function start(input) {
+	const points = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	let total = 0
 
+	input.forEach(x => {
+		const first = x.slice(0,x.length/2)
+		const second = x.slice(x.length/2)
+		let found = false
+		for(let i = 0; i < first.length; i++) {
+			const currentChar = first.charAt(i)
+			if(!found && second.indexOf(currentChar) != -1) {
+				total += (points.indexOf(currentChar)+1)
+				found = true
+			}
+		}
+	})
+
+	console.log(total)
 }
--- 2022/Day 3/Puzzle3-2.js
+++ 2022/Day 3/Puzzle3-2.js
@@ -10,5 +10,29 @@ const input = fs
 
 start(input)
 function start(input) {
+	const points = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	let total = 0
 
+	let groups = []
+    for (let i = 0; i < input.length; i += 3) {
+        groups.push(input.slice(i, i + 3));
+    }
+
+	groups.forEach(group => {
+		let found = false
+		let first = group[0]
+		let second = group[1]
+		let third = group[2]
+
+		for(let i = 0; i < first.length; i++) {
+			const currentChar = first.charAt(i)
+			if(!found && second.indexOf(currentChar) != -1 && third.indexOf(currentChar) != -1) {
+				total += (points.indexOf(currentChar)+1)
+				found = true
+			}
+		}
+	})
+
+
+	console.log(groups, total)
 }
