3e5a3cc99b04639103a1a813b25387d7e6ce6f68
ItsANoBrainer <itsanobrainergg@protonmail.com>
2022-12-04 00:36:13 -0500

    Day 4

--- 2022/Day 4/Puzzle4-1.js
+++ 2022/Day 4/Puzzle4-1.js
@@ -10,5 +10,20 @@ const input = fs
 
 start(input)
 function start(input) {
+	let total = 0
+	input.forEach(x => {
+		const set1 = x.split(',')[0]
+		const set1First = parseInt(set1.split('-')[0])
+		const set1Second = parseInt(set1.split('-')[1])
 
+		const set2 = x.split(',')[1]
+		const set2First = parseInt(set2.split('-')[0])
+		const set2Second = parseInt(set2.split('-')[1])
+
+		const isSet1InSet2 = (set1First >= set2First && set1Second <= set2Second)
+		const isSet2InSet1 = (set2First >= set1First && set2Second <= set1Second)
+		if (isSet1InSet2 || isSet2InSet1 ) total++
+	})
+
+	console.log(total)
 }
