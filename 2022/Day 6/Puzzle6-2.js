938a1a4f7cea3ebf75ded4f710c63452844b43d9
ItsANoBrainer <itsanobrainergg@protonmail.com>
2022-12-06 12:44:49 -0500

    2022 Day 6

--- 2022/Day 6/Puzzle6-2.js
+++ 2022/Day 6/Puzzle6-2.js
@@ -6,9 +6,26 @@ const input = fs
 	.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
 	.toString()
 	.trim()
-	.split('\r\n');
+	.split('\r\n')[0];
 
 start(input)
 function start(input) {
-
+	const markerNum = 14
+	for (let i = 0; i <= input.length - 1; i++) {
+		if (i <= input.length - markerNum) {
+			const section = input.slice(i, i + markerNum)
+			let hasDuplicate = false
+			for (let j = 0; j <= section.length - 1; j++) {
+				const charIndex = section.indexOf(section[j])
+				if (charIndex != j && charIndex != -1) {
+					hasDuplicate = true
+					break;
+				}
+			}
+			if (!hasDuplicate) {
+				console.log('Chars Processed', i + markerNum)
+				break;
+			}
+		}
+	}
 }
